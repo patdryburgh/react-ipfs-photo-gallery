6c51748475e3d3b65b73c702c6dada91a537d48f
Pat Dryburgh <hello@patdryburgh.com>
2019-11-19 12:37:37 -0800

    add photo gallery and ipfs user settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import UserSettingsForm from "./components/UserSettingsForm";
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import {CID, DEFAULT_CID, loadUserSettings} from "./ipfs/getUserSettings";
+import {Box, Container, Typography} from "@material-ui/core";
 import './App.css';
+import PhotoGallery from "./components/PhotoGallery";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const initialUserSettingsCid = localStorage.getItem("userSettingsCID") || DEFAULT_CID;
+    const [cid, setCID] = React.useState(initialUserSettingsCid);
+    const [userSettings, setUserSettings] = React.useState();
+    const [isLoadingUserSettings, setIsLoadingUserSettings] = React.useState(false);
+    const [error, setError] = React.useState();
+
+    const loadSettings = async (cid: CID) => {
+        try {
+            setIsLoadingUserSettings(true);
+            const p = await loadUserSettings(cid);
+            setUserSettings(p);
+        } catch (error) {
+            setIsLoadingUserSettings(false);
+            setError(error.toString());
+            return;
+        }
+        setIsLoadingUserSettings(false);
+        localStorage.setItem("userSettingsCID", cid)
+    };
+
+    React.useEffect(() => {
+        loadSettings(initialUserSettingsCid);
+    }, [initialUserSettingsCid]);
+
+    const theme = createMuiTheme(userSettings && {
+        palette: {
+            type: userSettings.theme,
+            primary: {
+                main: userSettings.primaryColor
+            },
+            secondary: {
+                main: userSettings.secondaryColor
+            },
+        },
+    });
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Box component={Container} mt={theme.spacing(1)}>
+                <Typography variant={'h1'} gutterBottom={true}>
+                    Hello{userSettings && ', ' + userSettings.username}!
+                </Typography>
+                <PhotoGallery />
+                <UserSettingsForm
+                    cid={cid}
+                    setCID={setCID}
+                    error={error}
+                    userSettings={userSettings}
+                    loadSettings={loadSettings}
+                    isLoadingUserSettings={isLoadingUserSettings}
+                />
+            </Box>
+        </ThemeProvider>
+    );
 }
 
 export default App;
